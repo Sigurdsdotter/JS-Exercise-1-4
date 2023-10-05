d2c10748163323b7940cc8962d564af5fa02e7c3
Amanda Copa <amandacopa@gmail.com>
2023-10-05 09:31:13 +0200

    Task

--- script.js
+++ script.js
@@ -41,6 +41,110 @@ console.log("Every 3rd char from:", mainString + "=", secondString);
 // Task 2
 
 function myFunction() {
+  let age; 
+  let txt = ""; 
+  let i = 1; //index how many times y have prompt wrong age
+  let prmptTxt = "What is your age?"; //prompt-box text
+  let messageElement = document.getElementById("g");
+  messageElement.innerHTML = txt;
+  
+  do {  // DO-WHILE OR WHILE -DO TO REPEATING loop , stop/end by 
+    age = parseInt(prompt(prmptTxt, 0));
+    // doing with switch
+    if (age >=18 && age <=22){
+      switch (age){   //using swich instead of if- elseif else. 
+                      //In some case can be more clear to read
+        case 18:
+        case 19:
+          txt = "You are young!";
+          console.log(txt);
+          break;
+        case 20:
+        case 21:
+          txt = "You are middle age!";
+          console.log(txt);
+          break;
+        case 22:
+          txt = "You are old!";
+          console.log(txt);
+          break;
+      }
+      messageElement.innerHTML = txt;
+      return;     //stop function when age is between 18 - 22
+    }
+    else{ 		  // 22 < age < 18  , prompt-box text changes 
+      prmptTxt= i++ +": input was " + age + ". Set age between 18 -22";
+    }
+  } while (age > 17 || age < 23);
+}
+
+
+/*
+//Task 2, same using only console.log
+//Prompt
+// npm install prompt-sync
+//https://www.youtube.com/watch?v=15EKPpm261M
+
+
+const prompt = require("prompt-sync")();
+
+let age = 0;
+do {
+  age = prompt("Set age : ");
+} while (age > 22 || age < 18);
+switch (age) {
+  case "18":
+    console.log(age,": Young");
+    break;
+  case "20":
+    console.log(age,": Middle age");
+    break;
+  case "22":
+    console.log(age,": Old");
+    break;
+  default:
+	break;
+}
+*/
+
+
+
+/*
+function myFunction() {
+  let age;
+  let messageElement = document.getElementById("g");
+
+  do {
+    age = parseInt(prompt("What is your age?", 0));
+  } while (age < 18 || age > 22);
+
+  let rightAge = "";
+
+  if (age >= 18 && age < 19) {
+    rightAge = "You are young!";
+  } else if (age >= 19 && age < 21) {
+    rightAge = "You are middle age!";
+  } else if (age >= 21 && age < 23) {
+    rightAge = "You are old!";
+  }
+
+  console.log(rightAge);
+
+  // Du kan också uppdatera meddelandeelementet på webbsidan om du vill visa meddelandet där.
+  messageElement.textContent = rightAge;
+}
+
+// Anropa funktionen när du vill använda den.
+myFunction();
+18
+*/
+
+
+/*
+//My first function
+
+
+function myFunction() {
   let age = parseInt(prompt("What is your age?", 0));
 
   let messageElement = document.getElementById("g");
@@ -59,6 +163,7 @@ function myFunction() {
     messageElement.innerHTML = "Thats a lovely age";
   }
 }
+*/
 
 
 
